be860b90095e991c54a871fea383098bc206a461
Samuel Kasman <samuel.kasman@creativedock.com>
2021-12-09 14:17:04 +0100

    yeah boiii

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>BeOnMind yeah boiii!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Welcome to BeOnMind!" />
       </main>
 
       <Footer />
