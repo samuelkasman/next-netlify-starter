947695ec2762b456e373cf1cc51c665691a7734e
Samuel Kasman <samuel.kasman@creativedock.com>
2021-12-16 12:17:32 +0100

    fix i18n and svg warnings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 import Head from 'next/head'
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import { Navigation } from '../components/layout/navigation'
 import { Footer } from '../components/sections/footer'
 import { PageLayout } from '../components/layout/pageLayout'
 import { Hero } from '../components/sections/hero'
 import { Boom } from '../components/sections/boom'
 import { Work } from '../components/sections/work'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 
 const Home: NextPage = () => {
+  const { t } = useTranslation('common')
   return (
     <>
       <Head>
-        <title>BeOnMind yeah boiii!</title>
+        <title>{t('meta.title')}</title>
+        <meta name="description" content={t('meta.description')} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -27,5 +31,11 @@ const Home: NextPage = () => {
     </>
   )
 }
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale as string, ['common'])),
+  },
+})
 
 export default Home
